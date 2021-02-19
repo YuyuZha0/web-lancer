56812fd06ffba25e3f6cdb37f81bb62da04920e1
fishzhao <fishzhao@tencent.com>
2021-02-19 10:30:51 +0800

    update file scan

--- src/main/java/com/tencent/weblancer/web/conf/FileInterfaceDefinitionSupplier.java
+++ src/main/java/com/tencent/weblancer/web/conf/FileInterfaceDefinitionSupplier.java
@@ -37,18 +37,21 @@ public final class FileInterfaceDefinitionSupplier
     List<DynamicInterfaceDefinition> dynamicInterfaceDefinitions = new ArrayList<>();
     for (String strPath : appMetaConfig.getInterfaceDefinitionPath()) {
       Path path = Paths.get(strPath);
+      if (!Files.isReadable(path)) {
+        log.warn("Path [{}] is not readable and will be skipped!", path);
+        continue;
+      }
       if (Files.isDirectory(path)) {
         try (Stream<Path> pathStream = Files.walk(path)) {
           pathStream
               .filter(Files::isRegularFile)
+              .filter(Files::isReadable)
               .forEach(p -> addInterfaceDefinition(dynamicInterfaceDefinitions, p));
         } catch (IOException e) {
           throw new RuntimeException(e);
         }
-      } else if (Files.isRegularFile(path)) {
+      } else if (Files.isRegularFile(path) && Files.isReadable(path)) {
         addInterfaceDefinition(dynamicInterfaceDefinitions, path);
-      } else {
-        log.warn("Path [{}] is not valid and will be skipped!", path);
       }
     }
     log.info("[{}] interface(s) detected.", dynamicInterfaceDefinitions.size());
